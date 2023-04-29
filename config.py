aa949f386521b762ae6b4140a3e394fcd19448b9
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-29 18:58:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "96b46175824223a33737657ab943fd6a"
 
 BOT_TOKEN = "5222572158:AAENHtTOnhWBh4UUZKTjq5ruMtil_4zRA_0"
 
-MONGO_DB_URI = "mongodb+srv://hevc:sucks@cluster0.mdnim6a.mongodb.net/?retryWrites=true&w=majority"
+MONGO_DB_URI = "mongodb+srv://720pp:fire@cluster0.ser1dtu.mongodb.net/?retryWrites=true&w=majority"
 INDEX_ID = -1001700435443
 
 UPLOADS_ID = -1001700435443
@@ -19,7 +19,7 @@ PROGRESS_ID = -1001700435443
 
 LINK_ID = -1001700435443
 
-STATUS_ID = 3
+STATUS_ID = 4
 
 SCHEDULE_ID = 3
 

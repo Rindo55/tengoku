ec0c052e63ba1cc716ae692dd10468a4801e1261
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-04-29 22:45:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_ID = "10247139"
 
 API_HASH = "96b46175824223a33737657ab943fd6a"
 
-BOT_TOKEN = "5222572158:AAENHtTOnhWBh4UUZKTjq5ruMtil_4zRA_0"
+BOT_TOKEN = "5706217182:AAELnRWjsHatV-yJ5MvNgb7yAz2-QF6EWf8"
 
 MONGO_DB_URI = "mongodb+srv://720pp:fire@cluster0.ser1dtu.mongodb.net/?retryWrites=true&w=majority"
 INDEX_ID = -1001700435443
